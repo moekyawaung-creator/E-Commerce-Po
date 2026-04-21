f359c63b4e4af50bb04e1c235d2d313cfc2230d2
moekyawaung-creator <moekyawaung1993@outlook.com>
2026-04-21 14:29:45 +0630

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ecommerce.pro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.ecommerce.pro.ui.theme.ECommerceProTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ECommerceProTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
